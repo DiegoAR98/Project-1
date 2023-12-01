9a95ddeef5fd44b721f1d3603d69f08e712532d1
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2023-12-01 18:50:10 -0500

    Fixed bugs on HTML and JS

--- assets/park-visit.js
+++ assets/park-visit.js
@@ -6,31 +6,29 @@ function saveLastVisitedPark(parkFileName) {
 // Function to redirect to the last visited park
 function redirectToLastVisitedPark() {
     var lastVisitedPark = localStorage.getItem('lastVisitedPark');
-    // Check if the current page is not the last visited page and is the main page
+    // Check if the last visited park is set and the current page is index.html
     if (lastVisitedPark && window.location.pathname.endsWith('index.html')) {
         window.location.href = lastVisitedPark;
     }
 }
 
-// Function to reset the last visited park when returning to the main page
-function resetLastVisitedPark() {
-    localStorage.removeItem('lastVisitedPark');
-}
-
+// Event listener for DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function () {
-    var parkLinks = document.querySelectorAll('.park-link');
-    
+    redirectToLastVisitedPark();
+
+    // Add event listeners to park links
+    var parkLinks = document.querySelectorAll('.dropdown-content a');
     parkLinks.forEach(link => {
-        link.addEventListener('click', function() {
+        link.addEventListener('click', function(event) {
             saveLastVisitedPark(this.getAttribute('href'));
         });
     });
 
-    // Reset last visited park if the 'return to main page' link is clicked
+    // Optional: Reset last visited park if 'Return to Main Page' link is clicked
     var returnMainLink = document.querySelector('.return-main-link');
     if (returnMainLink) {
-        returnMainLink.addEventListener('click', resetLastVisitedPark);
+        returnMainLink.addEventListener('click', function() {
+            localStorage.removeItem('lastVisitedPark');
+        });
     }
-  
-    redirectToLastVisitedPark();
 });
