a1d57880a20783160f3fb6859f8b2c9645b9d539
NicholasSewsahai <147281618+NicholasSewsahai@users.noreply.github.com>
2023-12-01 19:45:37 -0500

    youtube apis
    all youtube apis are added and properly working in live servers

---
+++ assets/youtube4.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var apiYTContainer = document.getElementById('api-yt');
+    if (apiYTContainer) {
+        var iframe = document.createElement('iframe');
+        iframe.width = '560';
+        iframe.height = '315';
+        iframe.src = 'https://www.youtube.com/embed/WiqCnBLDzFo';
+        iframe.title = 'YouTube video player';
+        iframe.frameborder = '0';
+        iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+        iframe.allowFullscreen = true;
+        apiYTContainer.appendChild(iframe);
+    }
+});    
